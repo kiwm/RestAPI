ebf09f6d099c4199604ecc148d728eac5d9ef3ea
Pedro <pedromaltapedrozo@gmail.com>
2022-02-24 20:17:17 -0300

    crud basic complet

--- src/main/java/com/RestAPI/TestAPI/controller/ClientController.java
+++ src/main/java/com/RestAPI/TestAPI/controller/ClientController.java
@@ -3,10 +3,10 @@ package com.RestAPI.TestAPI.controller;
 import com.RestAPI.TestAPI.model.Client;
 import com.RestAPI.TestAPI.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,9 +20,22 @@ public class ClientController {
     public List<Client> list() {
         return clientRepository.findAll();
     }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Client add(@RequestBody Client client) {
         return clientRepository.save(client);
+    }
+
+    @DeleteMapping
+    public void remove(@RequestBody Client client) {
+        clientRepository.delete(client);
+    }
+
+    @PutMapping
+    public void update(@RequestBody Client client) {
+        if(clientRepository.existsById(client.getId())){
+            clientRepository.save(client);
+        }
     }
 }
